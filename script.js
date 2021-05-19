dc65aae94e263adb5f0b9f9b30faa30b39da8235
Priyanshu-Khambalkar <83443943+Priyanshu-Khambalkar@users.noreply.github.com>
2021-05-19 11:35:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+function check_principal(){
+
+
+     //Get the values and calculate 
+     var raw_principal = parseFloat(document.getElementById("p").value);
+    
+     if (raw_principal == 0){
+        alert("enter a positive number");
+     }
+     else if (raw_principal < 0){
+        alert("enter a positive number");
+     }
+     
+     else{}
+
+    
+
+}
+
+
+
+
+
+function compute() {
+    
+    //Get the values and calculate 
+    var principal = parseFloat(document.getElementById("p").value);
+    var rate = parseFloat(document.getElementById("r").value);
+    var years = parseInt(document.getElementById("n").value);
+
+
+    var si = (principal * rate * years)/100;
+
+    var added_year = new Date().getFullYear() + years;
+
+    //Create the Interest text
+    document.getElementById("demo").innerHTML = "If you deposit <mark>" + principal + "</mark><br/>" +
+        "at an interest rate of <mark>" + rate + "%</mark><br/>" +
+        "You will receive an amount of <mark>" + si + "</mark><br/> " +
+        "in the year <mark>" + added_year +"</mark>";
+
+}
